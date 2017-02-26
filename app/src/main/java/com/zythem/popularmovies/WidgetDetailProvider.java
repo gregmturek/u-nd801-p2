19b95e7233aec55660fad772be0d6ab7bebc4cc2
Greg Turek <greg.m.turek@gmail.com>
2017-02-26 11:34:26 -0600

    Make basic content provider ListView widget

---
+++ app/src/main/java/com/zythem/popularmovies/WidgetDetailProvider.java
@@ -0,0 +1,104 @@
+package com.zythem.popularmovies;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.RemoteViews;
+
+public class WidgetDetailProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_detail);
+
+            // Set heading text
+            views.setTextViewText(R.id.widget_detail_heading, "Popular Movies");
+
+            // Create an Intent to launch MainActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget_detail_heading, pendingIntent);
+
+            // Set up the collection
+            setRemoteAdapter(context, views, appWidgetId/*, appWidgetManager*/);
+            boolean useDetailActivity = context.getResources()
+                    .getBoolean(R.bool.use_detail_activity);
+            Intent clickIntentTemplate = useDetailActivity
+                    ? new Intent(context, DetailActivity.class)
+                    : new Intent(context, MainActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_detail_list, clickPendingIntentTemplate);
+            views.setEmptyView(R.id.widget_detail_list, R.id.widget_detail_empty);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+//        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_detail_list);
+//        }
+    }
+
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views, int appWidgetId/*, AppWidgetManager appWidgetManager*/) {
+        Intent intent = new Intent(context, WidgetDetailRemoteViewsService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+/*
+        MyAppWidgetManager myAppWidgetManager = new MyAppWidgetManager(appWidgetManager);
+        intent.putExtra("MYAPPWIDGETMANAGER", myAppWidgetManager);
+*/
+        views.setRemoteAdapter(R.id.widget_detail_list, intent);
+    }
+
+/*
+    public class MyAppWidgetManager implements Serializable {
+        private AppWidgetManager appWidgetManager;
+
+        public MyAppWidgetManager(AppWidgetManager appWidgetManager) {
+            this.appWidgetManager = appWidgetManager;
+        }
+
+        public AppWidgetManager getAppWidgetManager() {
+            return appWidgetManager;
+        }
+
+        public void setAppWidgetManager(AppWidgetManager appWidgetManager) {
+            this.appWidgetManager = appWidgetManager;
+        }
+    }
+*/
+
+/*
+    @Override
+    public void onAppWidgetOptionsChanged(Context c, AppWidgetManager manager, int appWidgetId, Bundle newOptions) {
+
+        Bundle options = manager.getAppWidgetOptions(appWidgetId);
+
+        int minWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int maxWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+
+        int minHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int maxHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+    }
+*/
+}
---
+++ app/src/main/java/com/zythem/popularmovies/WidgetDetailRemoteViewsService.java
@@ -0,0 +1,166 @@
+package com.zythem.popularmovies;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.io.IOException;
+
+public class WidgetDetailRemoteViewsService extends RemoteViewsService {
+    private static final String LOG_TAG = WidgetDetailRemoteViewsService.class.getSimpleName();
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(final Intent intent) {
+
+        return new RemoteViewsFactory() {
+            private Cursor c = null;
+            private int mWidth, mHeight, mAppWidgetId;
+            private AppWidgetManager mAppWidgetManager;
+
+            @Override
+            public void onCreate() {
+//                mAppWidgetManager = (AppWidgetManager) getSystemService(Context.APPWIDGET_SERVICE);
+                mAppWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
+                mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                        AppWidgetManager.INVALID_APPWIDGET_ID);
+
+                Bundle options = mAppWidgetManager.getAppWidgetOptions(mAppWidgetId);
+
+                int minWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+                int maxWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+
+/*
+                int minHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+                int maxHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+*/
+
+                if (getResources().getBoolean(R.bool.portrait_orientation)) {
+                    mWidth = (int) (minWidth * getResources().getDisplayMetrics().density);
+                    mHeight = (int) ((minWidth * 3 / 2) * getResources().getDisplayMetrics().density);
+                } else {
+                    mWidth = (int) (maxWidth * getResources().getDisplayMetrics().density);
+                    mHeight = (int) ((maxWidth * 3 / 2) * getResources().getDisplayMetrics().density);
+                }
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (c != null) {
+                    c.close();
+                }
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+                Uri uriType = MovieContentProvider.MostPopular.MOVIES;
+                c = getContentResolver().query(uriType, null, null, null, null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (c != null) {
+                    c.close();
+                    c = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return c == null ? 0 : c.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        c == null || !c.moveToPosition(position)) {
+                    return null;
+                }
+                final RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_detail_item);
+
+                // Extract the data from the Cursor
+
+                MovieDataToPass data = new MovieDataToPass();
+                data.mTitle = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_TITLE));
+                data.mImagepath = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_IMAGEPATH));
+                data.mDate = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_DATE));
+                data.mRating = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_RATING));
+                data.mId = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_ID));
+                data.mOverview = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_OVERVIEW));
+                data.mImagepath2 = c.getString(c.getColumnIndex(MostPopularColumns.MOVIE_IMAGEPATH_2));
+
+                // Add the data to the RemoteViews
+
+                Bitmap bitmap = null;
+                if (data.mImagepath != null && !data.mImagepath.isEmpty()) {
+                    //Run Picasso on the main thread
+                    try {
+                        bitmap = Picasso.with(WidgetDetailRemoteViewsService.this)
+                                .load(data.mImagepath)
+                                .resize(mWidth, 0)
+                                .get();
+                        if (bitmap != null) {
+                            views.setImageViewBitmap(R.id.widget_detail_movie_image, bitmap);
+                        }
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error retrieving image from " + data.mImagepath, e);
+                    }
+                }
+
+                if (bitmap != null) {
+                    views.setTextViewText(R.id.widget_detail_movie_title, data.mTitle);
+                } else {
+                    views.setTextViewText(R.id.widget_detail_movie_title_no_image, data.mTitle);
+                }
+
+                final Intent fillInIntent = new Intent();
+                fillInIntent.putExtra("THE_DATA", Parcels.wrap(data));
+                views.setOnClickFillInIntent(R.id.widget_detail_item, fillInIntent);
+                return views;
+            }
+
+/*
+            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            private void setRemoteContentDescription(RemoteViews views, String description) {
+                views.setContentDescription(R.id.widget_icon, description);
+            }
+*/
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_detail_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (c.moveToPosition(position))
+                    return c.getLong(0);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
