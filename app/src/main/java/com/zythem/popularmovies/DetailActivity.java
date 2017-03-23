7b0d557770d021e10444975e5df4c6871cca1a6c
Greg Turek <greg.m.turek@gmail.com>
2017-03-23 14:14:41 -0500

    Fix up navigation functioning after launch from widgets

--- app/src/main/java/com/zythem/popularmovies/DetailActivity.java
+++ app/src/main/java/com/zythem/popularmovies/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.zythem.popularmovies;
 
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,9 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.parceler.Parcels;
@@ -111,19 +114,46 @@ public class DetailActivity extends AppCompatActivity {
             // set any flags or extras that you need.
             // If you are reusing the previous Activity (i.e. bringing it to the top
             // without re-creating a new instance) set these flags:
-            // (The below is not needed because this activity is set to SINGLE_TOP in the manifest.)
+            // GMT // Below is not needed because this activity is set to SINGLE_TOP in the manifest.
 //            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             // if you are re-using the parent Activity you may not need to set any extras
 //            i.putExtra("someExtra", "whateverYouNeed");
-/*
-        } else { // (This is not needed because null defaults to what is specified in the manifest.)
-            i = new Intent(this, MainActivity.class);
+            // GMT // Below is not needed because null defaults to what is specified in the manifest.
+//        } else {
+//            i = new Intent(this, MainActivity.class);
             // same comments as above
-*/
         }
         return i;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent) || isTaskRoot()) {
+                    // This activity is NOT part of this app's task,
+                    // GMT // is the root of a task (initial launch of application
+                    // GMT // or application was previously terminated through task manager),
+                    // so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // GMT // This is not per guidelines but is the desired behavior.
     @Override
     public void onBackPressed() {
         String parent = getIntent().getStringExtra("THE_PARENT");
